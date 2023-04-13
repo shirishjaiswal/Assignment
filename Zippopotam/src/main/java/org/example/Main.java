5cf6c4331c35cd9863d1dd679fe9ad4e25265c83
shirishjaiswal <shirishjaiswal03@gmail.com>
2023-04-13 10:55:47 +0530

    This is Zippopatam Assignment

---
+++ Zippopotam/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        URL setURL = new URL("https://api.zippopotam.us/us/33162");
+        HttpURLConnection httpConnection = (HttpURLConnection) setURL.openConnection();
+        httpConnection.setRequestMethod("GET");
+
+        int responseCode = httpConnection.getResponseCode();
+
+        if(responseCode == httpConnection.HTTP_OK) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()))) {
+                StringBuilder jsonData = new StringBuilder();
+                String readLine = null;
+
+                while ((readLine = in.readLine()) != null) {
+                    jsonData.append(readLine);
+                }
+
+                JSONObject jsonObj = new JSONObject(jsonData.toString());
+                System.out.println(jsonObj.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
