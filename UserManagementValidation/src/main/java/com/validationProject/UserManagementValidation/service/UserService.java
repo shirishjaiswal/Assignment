6156a172a9ccaa57a1de3e3de056187fe52b8efe
shirishjaiswal <shirishjaiswal03@gmail.com>
2023-04-28 21:12:47 +0530

    Assignment on Validation

---
+++ UserManagementValidation/src/main/java/com/validationProject/UserManagementValidation/service/UserService.java
@@ -0,0 +1,62 @@
+package com.validationProject.UserManagementValidation.service;
+
+import com.validationProject.UserManagementValidation.model.User;
+import com.validationProject.UserManagementValidation.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    private List<User> userList;
+
+    private User byId (String id) {
+        userList = userRepo.getUsers();
+        User user = null;
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getId().equals(id)) {
+                user = userList.get(i);
+            }
+        }
+        return user;
+    }
+
+    public User getUser(String id) {
+        return byId(id);
+    }
+
+    public boolean addUser (User user) {
+        return userRepo.addUser(user);
+    }
+
+    public boolean deleteUser (String id) {
+        User user = byId(id);
+        return userRepo.deleteUser(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepo.getUsers();
+    }
+
+    public boolean updateUser(String id, User user) {
+        User toFind = byId(id);
+        if(toFind != null) {
+            user.setId(toFind.getId());
+            if(user.getName() == null) user.setName(toFind.getName());
+            if(user.getEmail() == null) user.setEmail(toFind.getEmail());
+            if(user.getMobileNo() == null) user.setMobileNo(toFind.getMobileNo());
+            if(user.getDateOfBirth() == null) user.setDateOfBirth(toFind.getDateOfBirth());
+        }
+        boolean isDeleted = deleteUser(id);
+        boolean isAdded = false;
+        if(isDeleted) {
+            isAdded = addUser(user);
+        }
+        return isAdded;
+    }
+}
---
+++ UserManagementValidation/src/test/java/com/validationProject/UserManagementValidation/UserManagementValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validationProject.UserManagementValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
